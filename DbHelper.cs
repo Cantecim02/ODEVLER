388bd386a7ef01f68d272543644fe843e2bcf1d1
Mevlüt Can tecim <cantecim@outlook.com>
2025-06-23 23:31:45 +0300

    Add files via upload

---
+++ DbHelper.cs
@@ -0,0 +1,403 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Ders25_AlisVeris.Data;
+using Ders25_AlisVeris.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+
+namespace Ders25_AlisVeris
+{
+
+    public class DbHelper
+    {
+        private AddDbContext _db = new AddDbContext();
+        #region Kullanicilar
+        //kullanıcı açma
+        public string KullanıcıEkle(Kullanici data) //ile kullanıcı ekleme işlemi yapılır
+        {
+
+            _db.Kullanicilar.Add(data);
+            _db.SaveChanges();
+            return $"{data.AdSoyad} adlı kullanıcı başarıyla eklendi.";
+
+        }
+        //public string siparisolustur(Siparis data)
+        //{
+        //    _db.Siparisler.Add(data); //sipariş ekleme işlemi yapılır
+        //    _db.SaveChanges(); //veritabanına kaydedilir
+        //    return $"{data.Kullanici.AdSoyad} adlı kullanıcı için sipariş başarıyla oluşturuldu.";
+
+        //}
+
+
+        #endregion
+        #region Urun Kategorileri
+
+        public string UrunkategoriEkle(UrunKategori data)
+        {
+
+            _db.UrunKategoriler.Add(data);
+            _db.SaveChanges();
+            return $"{data.Adi}" + " adlı ürün kategorisi başarıyla eklendi.";
+        }
+
+
+
+
+        #endregion
+
+        //Sıraya göre Listeleme
+
+        public List<UrunKategori> UrunKategoriListele() //urunkategorileri listeleme işlemi yapılır ve bir metot oluşturulur
+        {
+            return _db.UrunKategoriler.OrderBy(P => P.Sira).ToList(); //orderby ile sıralama işlemi yapılır
+            //tolist ile listeleme işlemi yapılır 
+        }
+
+
+
+        #region Urunler
+        //kategori ıd sine göre o kategoriye ait ürünler lsitelenecek
+
+        public List<Urun> GetUrunlerByKategori()
+        {
+            int kategoriId = Convert.ToInt32(Console.ReadLine()); // kullanıcıdan kategori id si alınır
+            return _db.Urunler.Where(p => p.KtegoriId == kategoriId).ToList(); //where ile kategori id sine göre filtreleme işlemi yapılır
+
+
+
+
+        }
+
+
+
+
+        #endregion
+
+
+
+
+
+
+
+
+
+
+
+        //ürün tablosu örnekleri 
+
+
+        //public List<int> TumUrunIdleri()
+        //{
+        //    return _db.Urunler.Select(u => u.Id).ToList();
+        //}
+
+
+        //urun listesindeki urunlerin adları 
+        //public List<string> tumisimler()
+        //{
+        //    return _db.Urunler.Select(p => p.Adi).ToList(); //ürünlerin isimlerini listeleme işlemi yapılır
+
+        //}
+
+
+        //urun tablosundaki urunlerin birim fiyatları
+
+        //public List<double> TumUrunBirimFiyatlari()
+        //{
+        //    return _db.Urunler.Select(p => p.BirimFiyat).ToList(); //ürünlerin birim fiyatlarını listeleme işlemi yapılır
+        //}
+
+
+        // urunlerin aktiflik durumunu sorgulama
+        //public List<Urun> AktifUrunler()
+        //{
+        //    return _db.Urunler.Where(p => p.IsPassive == false).ToList(); //IsPassive false olan ürünleri listeleme işlemi yapılır
+
+
+        //    public List<int> stoklar()
+        //    {
+        //        return _db.Urunler.Select(p => p.Stok).ToList(); //ürünlerin stoklarını listeleme işlemi yapılır
+        //}
+        //}
+
+
+
+        //urunlerin barkod numarasını getirme işlemi 
+
+        //public List<Urun> Urunbarkodları()
+        //{
+        //    return _db.Urunler.Include(u => u.Kategori).OrderBy(p => p.BarkodNo).ToList(); 
+        //nclude ile kategori bilgisi de dahil edilerek barkod numarasına göre sıralama işlemi yapılır:not : 
+        //}
+
+
+        //urunleri kategori ıd sine göre sıralama 
+        //public List<Urun> urunkategoriıd()
+        //{ 
+        //  return _db.Urunler.Include(u => u.Kategori).OrderBy(p => p.KtegoriId).ToList(); //ürünlerin kategori id sine göre sıralama işlemi yapılır
+        //}
+
+
+
+        //urunlerin sıra bilgisini alma 
+
+
+        //public List<Urun> urunlerinsirasi()
+        //{
+        //    return _db.Urunler.OrderBy(p => p.Sira).ToList(); //ürünlerin sıra bilgisine göre sıralama işlemi yapılır
+        //}
+
+
+        //olusturulmatarihlerini alma
+
+        //public List<DateTime> UrunOluşturulmaTarihi()
+        //{
+        //    return _db.Urunler.Select(p => p.OlsuturulmaTarihi).ToList(); //ürünlerin oluşturulma tarihine göre sıralama işlemi yapılır
+        //}
+
+
+        // urunkategori tablosundan ıd lerini çekme 
+
+        //public List<int> UrunKategoriIdleri()
+        //{
+        //    return _db.UrunKategoriler.Select(p => p.Id).ToList(); //ürün kategorilerinin id'lerini listeleme işlemi yapılır
+        //}
+
+
+        //urunkategoriden adı(string) işlemi 
+
+
+        //public List<UrunKategori> urunkategoriAdlari()
+        //{
+        //    return _db.UrunKategoriler.Select(p => new UrunKategori
+        //    {
+        //        Id = p.Id,
+        //        Adi = p.Adi
+        //    }).ToList(); //ürün kategorilerinin adlarını listeleme işlemi yapılır
+        //}
+
+
+
+
+        //urunlerkategoriden kategorilerin aktif pasif durumlarını alma işlemi
+
+
+        //public List<UrunKategori> urunkategoridurumları()
+        //{ 
+        //    return _db.UrunKategoriler.Select(p => new UrunKategori
+        //    {
+        //        Id = p.Id,
+        //        Adi = p.Adi,
+        //        IsPassive = p.IsPassive
+        //    }).ToList(); //ürün kategorilerinin aktif pasif durumlarını listeleme işlemi yapılır
+
+
+        //}
+
+
+
+
+
+        // UrunlerKategori Açıklama (string)
+
+        //public List<UrunKategori> urunAcıklama()
+        // {
+
+        //     return _db.UrunKategoriler.OrderBy(p=>p.Aciklama).ToList(); //ürün kategorilerinin açıklamalarını listeleme işlemi yapılır
+
+
+        // }
+
+
+
+        //urunkategori Kategori No
+
+        //public List<UrunKategori> urunkategorino()
+        //{
+        //    return _db.UrunKategoriler.ToList();
+
+        //}
+
+
+        //urunkategori Sira (int)
+
+        //public List<UrunKategori> urunKategoriSira()
+        //{
+        //return _db.UrunKategoriler.OrderBy(p => p.Sira).ToList(); 
+        //}
+
+
+
+
+
+        //urunkategori OluşturulmaTarihi (datetime)
+
+        //public List<UrunKategori> urunkategoridatetime()
+        //{ 
+        //  return _db.UrunKategoriler.OrderByDescending(p => p.OlsuturulmaTarihi).ToList(); 
+
+        //}
+
+
+        //--------------------------------------------------
+        //kullanıcılar listesinden ıd leri alma işlemi 
+
+
+        //public List<int > tumkullancıIdleri()
+        //{
+
+        //    return _db.Kullanicilar.Select(p => p.Id).ToList(); 
+
+
+
+        //}
+
+
+
+
+        // kullanıcılar AdSoyad alma işlemi 
+
+        //public List<Kullanici> tumKullaniciAdlari()
+        //{
+        //    return _db.Kullanicilar.OrderBy(p =>p.AdSoyad).ToList(); //kullanıcıların adlarını sıralı bir şekilde listeleme işlemi yapılır
+
+        //}
+
+
+
+        // kullanıcılar mail alma işlemi 
+
+        //public List<Kullanici> kullanıcımail()
+        //{
+        //    return _db.Kullanicilar.Select(p => new Kullanici
+        //    {
+        //        Id = p.Id,
+        //        Email = p.Email
+        //    }).ToList(); //kullanıcıların mail adreslerini listeleme işlemi yapılır
+
+        //}
+
+
+        //kullanıcılar Şifre alma işlemi
+
+
+        //public List<Kullanici> kullanicisifre()
+        //{
+        //    return _db.Kullanicilar.Select(p => new Kullanici
+        //    {
+        //        Id = p.Id,
+        //        Sifre = p.Sifre,
+        //        AdSoyad = p.AdSoyad 
+        //    }).ToList(); 
+        //}
+
+
+
+        //kullanıcılar IsPassive (bool) işlemi 
+
+
+        //public List<Kullanici> kullaniciIsPassive()
+        //{
+        //    return _db.Kullanicilar.Select(p => new Kullanici
+        //    {
+        //        Id = p.Id,
+        //        AdSoyad = p.AdSoyad,
+        //        IsPassive = p.IsPassive
+        //    }).ToList(); //kullanıcıların aktif pasif durumlarını listeleme işlemi yapılır
+        //}
+
+
+        // kullanıcılar OluşturulmaTarihi (datetime)
+
+
+        //public List<Kullanici> kullanıcıtarih()
+        //{ 
+        //  return _db.Kullanicilar.Where(p => p.OluşturulmaTarihi > DateTime.Now.AddYears(-1)).ToList(); //son bir yılda oluşturulan kullanıcıları listeleme işlemi yapılır
+
+        //}
+
+
+        //--------------------------------------------------
+        //siparisler tablosundan veri çekme metotları
+
+        //public List<Siparis> siparisID()
+        //{ 
+        //  return _db.Siparisler.Select(p => new Siparis
+        //  {
+        //      Id = p.Id,
+        //      SiparisNo = p.SiparisNo
+        //  }).ToList(); 
+        //}
+
+
+
+        //siparişler tablosu Sipariş No (string)
+
+
+        //public List<Siparis> siparisno()
+        //{
+        //    return _db.Siparisler.OrderByDescending(p => p.SiparisNo).ToList(); 
+
+
+        //}
+
+
+
+        //siparişler tablosu OluşturulmaTarihi (datetime)
+
+        //public List<Siparis> siparisOluşturulmaTarihi()
+        //{
+        //    return _db.Siparisler
+        //  .OrderByDescending(p => p.OlusturulmaTarihi)
+        //  .ThenByDescending(p => p.Toplam) //toplam tutara göre büyükten küçüğe sıraladık 
+        //  .ToList();
+
+        //}
+
+        //siparisler tablosu Kullanıcı Id (int)
+
+        //public List<Siparis> kullanıcııd()
+        // {
+        //     return _db.Siparisler.Where(p => p.KUlllanıcıId > 0).ToList(); 
+
+
+        // }
+
+
+        //siparisler tablosu Toplam (double)
+
+        //public List<Siparis> siparislerToplam()
+        //{
+        //    return _db.Siparisler
+        //       .OrderByDescending(p => p.Toplam)
+        //       .Include(p => p.Kullanici)
+        //       .ToList();
+
+        //}
+
+
+        //---------------------------------------------
+
+
+        //siparisitem işlemleri 
+
+        //siparileri tabloya iletemedim 
+
+
+      
+
+
+
+    }
+}
+//linq sorgularına örnekler 
+// bir kullanıcıya ait tüm alışverişlerin numaraları listeleme 
+
---
+++ Program.cs
@@ -0,0 +1,375 @@
+﻿using System.Data.Common;
+using System.Diagnostics.Contracts;
+using Ders25_AlisVeris.Models;
+
+namespace Ders25_AlisVeris
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DbHelper dbHelper = new DbHelper();
+
+            //Siparis siparis = new Siparis();
+            //siparis.SiparisNo = "#25645";
+            //siparis.Id = 1; // Kullanıcı ID'si, örneğin 1 numaralı kullanıcı için sipariş oluşturuluyor
+            //siparis.Toplam = 150.75; // Sipariş toplam tutarı
+            //siparis.KUlllanıcıId = 1; // Siparişi oluşturan kullanıcının ID'si
+            ////Kullanici kullanıcı = new Kullanici();
+
+            //kullanıcı.AdSoyad = "Ahmet Yılmaz";
+            //kullanıcı.Sifre= "123456";
+            //kullanıcı.Email = "ahmetyılmaz@outlook.com";
+
+
+            //string sonuc = dbHelper.KullanıcıEkle(kullanıcı);
+            //Console.WriteLine(sonuc);
+
+            //---------------------------------
+
+            //UrunKategori Kategori= new UrunKategori();
+            //Kategori.Adi = "Elektronik";
+            //Kategori.Sira = 1;
+            //Kategori.UrunNo = "ELK-001";
+
+
+
+            //string sonuc = dbHelper.UrunkategoriEkle(Kategori);
+            //Console.WriteLine(sonuc);
+            //----------------------------------
+
+
+
+
+
+
+            //------------------------------------
+
+
+            // var listeUrunKategorileri = dbHelper.UrunKategoriListele();
+            //foreach (var item in listeUrunKategorileri)
+            //{
+            //    Console.WriteLine($"Kategori Adı: {item.Adi}, Sıra: {item.Sira}, Ürün No: {item.UrunNo}, Oluşturulma Tarihi: {item.OlsuturulmaTarihi}");
+
+            //}
+
+
+            //--------------------------------
+
+
+            //var urunler =dbHelper.GetUrunlerByKategori(); //kategori id sine göre ürünleri listeleme işlemi yapılır
+            //// var ile urunler adında bir değişken tanımlanır ve dbHelper sınıfından GetUrunlerByKategori metodu çağrılır
+            //foreach (var item in urunler) //foreach döngüsü ile urunler listesindeki her bir ürüne erişilir
+            //{
+
+            //    Console.WriteLine(item.Adi);
+            //}
+
+
+            //var tumUrunIdleri = dbHelper.TumUrunIdleri();
+            //foreach (var id in tumUrunIdleri)
+            //{
+            //    Console.WriteLine(id);
+            //}
+
+
+
+            //urun tablosundaki urunlerin adları 
+            //var tumurunadlari = dbHelper.tumisimler();
+            //foreach(var adi in tumurunadlari)
+            //{
+            //    Console.WriteLine(adi);
+            //}
+
+            //urun tablosundaki BİRİMFİYAT  ları double yazma 
+
+            //var tumUrunFiyatlari = dbHelper.TumUrunBirimFiyatlari();
+            //foreach (var item in tumUrunFiyatlari)
+            //{
+
+            //    Console.WriteLine(item);
+            //}
+
+
+
+            //tum urunlerin durumu 
+            //var tumUrunDurumlari = dbHelper.AktifUrunler();
+            //foreach(var item in tumUrunDurumlari)
+            //{
+            //    Console.WriteLine($"Ürün Adı: {item.Adi}, Durumu: {(item.IsPassive ? "Pasif" : "Aktif")}, Stok: {item.Stok}, Barkod No: {item.BarkodNo}, Oluşturulma Tarihi: {item.OlsuturulmaTarihi}"); // IsPassive true ise Pasif, false ise Aktif olarak gösterilir
+            //}
+
+            //urunlerin stokları 
+
+            //var urunstokları = dbHelper.stoklar();
+            //foreach (var item in urunstokları)
+            //{
+            //    Console.WriteLine($" Urunlerin Stok durumu : {item}  ");
+            //}
+
+
+            //var barkodno = dbHelper.Urunbarkodları(); // 1 numaralı ürünün barkod numarasını getirir
+            //foreach(var item in barkodno)
+            //{
+            //    Console.WriteLine($"Ürün Barkod No: {item.BarkodNo}, Ürün Adı: {item.Adi}, Kategori: {item.Kategori.Adi}, Oluşturulma Tarihi: {item.OlsuturulmaTarihi}");
+            //}
+
+
+
+            //urunleri kategori İD sine göre sıralama 
+
+            //var urunkategoriId = dbHelper.urunkategoriıd(); //ürünlerin kategori id sine göre sıralama işlemi yapılır
+            // foreach (var item in urunkategoriId)
+            //{
+            //    Console.WriteLine($"Ürün Adı: {item.Adi}, Kategori ID: {item.KtegoriId}");
+            //}
+
+
+
+            //urunkerin sıra bilgisini alma 
+
+            //var urunlerinsirasi = dbHelper.urunsırabilgisi(); //ürünlerin sıra bilgilerini listeleme işlemi yapılır
+            //foreach (var item in urunlerinsirasi)
+            //{
+            //    Console.WriteLine($"Ürün Sırası: {item}"); //ürünlerin sıra bilgilerini yazdırma işlemi yapılır
+            //}
+
+
+
+            //urunlerin sırası 
+
+
+            //var urunlersirasi = dbHelper.urunlerinsirasi(); //ürünlerin sıra bilgisine göre sıralama işlemi yapılır
+            //foreach (var item in urunlersirasi)
+            //{
+            //    Console.WriteLine($"Ürün Sırası: {item.Sira}"); //ürünlerin sıra bilgilerini yazdırma işlemi yapılır
+
+
+
+            //}
+
+
+
+            //olusuturulma tarihi alma 
+            //var olusturulmatarihi = dbHelper.UrunOluşturulmaTarihi(); //ürünlerin oluşturulma tarihini listeleme işlemi yapılır
+            //foreach (var item in olusturulmatarihi)
+            //{
+            //    Console.WriteLine(item);
+
+            //}
+
+            //urunkategoriye açıklama çekme işlemi
+
+            //var urunkategoriId = dbHelper.UrunKategoriIdleri();
+            //foreach (var item in urunkategoriId)
+            //{
+            //    Console.WriteLine(item);
+
+
+
+
+
+
+            //}
+
+
+
+
+            // urunkategoriden adı(string) işlemi 
+
+            //var urunkategoriadi = dbHelper.urunkategoriAdlari();
+            //foreach (var item in urunkategoriadi)
+            //{
+            //    Console.WriteLine($"{item.Adi} , {item.Id}");
+
+            //}
+
+            //urunkategoriden aktif pasif durumlarını alma işlemi 
+
+            //var urunkategoriıspassive = dbHelper.urunkategoridurumları();
+            //foreach (var item in urunkategoriıspassive)
+            //{
+            //   Console.WriteLine($"{item.IsPassive}"); 
+
+            //}
+
+
+
+
+
+            // UrunlerKategori Açıklama (string)
+
+
+
+            //var urunaAciklamalari = dbHelper.urunAcıklama();
+            //foreach (var item in urunaAciklamalari)
+            //{
+            //    Console.WriteLine($"Ürün Adı: {item.Adi}, Açıklama: {item.Aciklama}");
+            //}
+
+
+            //urunkategori Kategori No
+
+
+            //var urunkategorino = dbHelper.urunkategorino();
+            //foreach (var item in urunkategorino)
+            //{
+            //    Console.WriteLine($"Kategori Adı: {item.Adi}, Kategori No: {item.UrunNo}");
+            //}
+
+
+
+
+
+
+
+
+
+
+            //urunkategori Sira (int)
+
+
+            //var urunkategoriSira = dbHelper.urunKategoriSira();
+            //foreach (var item in urunkategoriSira)
+            //{
+            //    Console.WriteLine($"Kategori Adı: {item.Adi}, Sıra: {item.Sira}");
+            //}
+
+
+
+
+
+            //urunkategori OluşturulmaTarihi (datetime)
+
+
+            //var kategoritarihleri = dbHelper.urunkategoridatetime();
+            //foreach (var item in kategoritarihleri)
+            //{
+            //    Console.WriteLine($"{item.OlsuturulmaTarihi}");
+            //}
+
+
+
+
+            //--------------------------------------------------
+            //kullanıcılar listesinden ıd leri alma işlemi 
+
+
+
+            //var kullanıcılarID = dbHelper.tumkullancıIdleri();
+            //foreach (var item in kullanıcılarID)
+            //{
+            //    Console.WriteLine($"Kullanıcı ID: {item} "); 
+            //}
+
+            // kullanıcılar AdSoyad alma işlemi 
+
+            //var tumkullancıad =dbHelper.tumKullaniciAdlari();
+
+            //foreach (var item in tumkullancıad)
+            //{
+            //    Console.WriteLine($"Kullanıcı Adı: {item.AdSoyad}");
+            //}
+
+
+            // kullanıcılar mail alma işlemi 
+
+
+            //var kullanıcılarmail = dbHelper.kullanıcımail();
+            //foreach (var item in kullanıcılarmail)
+            //{
+            //    Console.WriteLine($"Kullanıcı Mail: {item.Email}");
+            //}
+
+
+
+
+            //kullanıcılar Şifre alma işlemi
+
+            //var kullanıcısifre = dbHelper.kullanicisifre();
+            //foreach (var item in kullanıcısifre)
+            //{
+            //    Console.WriteLine($"{item.AdSoyad}  Kullanıcısının Şifresi: {item.Sifre}"); 
+            //}
+
+            //kullanıcılar IsPassive (bool) işlemi 
+
+            //var kullanıcıdurumu =dbHelper.kullaniciIsPassive();
+
+            //foreach (var item in kullanıcıdurumu)
+            //{
+            //    Console.WriteLine($"Kullanıcı Adı: {item.AdSoyad}, Durumu: {(item.IsPassive ? "Pasif" : "Aktif")}"); 
+            //}
+
+
+
+            //var kullanıcıTrih =dbHelper.kullanıcıtarih();
+            //foreach (var item in kullanıcıTrih)
+            //{
+            //    Console.WriteLine($"Kullanıcı Adı: {item.AdSoyad}, Oluşturulma Tarihi: {item.OluşturulmaTarihi}"); 
+            //}
+
+
+            //--------------------------------------------------
+            //siparisler tablosundan veri çekme metotları
+
+
+            //var siparislerıd = dbHelper.siparisID();
+            //foreach (var item in siparislerıd)
+            //{
+            //    Console.WriteLine($"Sipariş ID: {item.Id}"); 
+            //}
+
+
+            //siparişler tablosu Sipariş No (string)
+
+
+
+            //var siparisNo = dbHelper.siparisno();
+            //foreach (var item in siparisNo)
+            //{
+            //    Console.WriteLine($"Sipariş No: {item.SiparisNo}"); 
+            //}
+
+
+            //siparişler tablosu OluşturulmaTarihi (datetime)
+
+
+            //var siparisolusturmatarihi = dbHelper.siparisOluşturulmaTarihi();
+            //foreach (var item in siparisolusturmatarihi)
+            //{
+
+            //  Console.WriteLine($"Sipariş Oluşturulma Tarihi: {item.OlusturulmaTarihi}  Sipariş Toplamı : {item.Toplam}"); 
+
+            //}
+
+
+            //siparisler tablosu Kullanıcı Id (int)
+
+            //var kullanıcııd =dbHelper.kullanıcııd();
+            //foreach (var item in kullanıcııd)
+            //{
+            //    Console.WriteLine($"Kullanıcı ID: {item.KUlllanıcıId}");
+            //}
+
+
+            //siparisler tablosu Toplam (double)
+
+            //var siparisToplam = dbHelper.siparislerToplam();
+            //foreach (var item in siparisToplam)
+            //{
+            //    Console.WriteLine($"  {item.Id} Id'li Kullanıcının Sipariş Toplamı: {item.Toplam}"); 
+            //}
+
+            //---------------------------------------------
+
+
+            //siparisitem işlemleri 
+
+
+
+
+
+        }
+    }
+}
